375641f5759347105523097895230643ab9c97b6
Tang <tangz@tumblr.com>
2015-08-23 22:17:21 -0700

    basic function

---
+++ tip/tip/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Tang Zhang on 8/23/15.
+//  Copyright (c) 2015 Tang Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        
+        var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format:"$%.2f",tip)
+        totalLabel.text = String(format:"$%.2f",total)
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
